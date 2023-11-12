cb8e9037fa6cd4889a15f9890e70aea01b3cbff8
ojAntoni <zezyulin90@gmail.com>
2023-11-12 10:52:27 +0100

    .

--- src/test/java/com/example/pzuseractivityservice/PzUserActivityServiceApplicationTests.java
+++ src/test/java/com/example/pzuseractivityservice/PzUserActivityServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PzUserActivityServiceApplicationTests {
 
     @Test
-    void contextLoadss() {
+    void contextLoads() {
     }
 
 }
